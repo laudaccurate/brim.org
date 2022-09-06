0d72bc799a9452f60df9f4f7add36ed689c4f7b4
Laud Gilbert <laudaccurate@gmail.com>
2022-09-06 17:13:55 +0000

    contact page laid out

---
+++ pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ContactUs from "../components/Contact";
+import Layout from "../components/Layout";
+
+export default function ContactPage() {
+  return (
+    <Layout>
+      <div className="">
+        <ContactUs />
+      </div>
+    </Layout>
+  );
+}
